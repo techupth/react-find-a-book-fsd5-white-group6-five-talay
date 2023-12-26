f84c056f7b97c21cd0c116a9ab46e4ede1b79614
Pacharadanai <pacharadanaipinake@gmail.com>
2023-12-26 14:15:51 +0700

    feat: add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [findBook, setFindBook] = useState("");
+  const [bookList, setBookList] = useState([]);
+
+  const findBookName = async () => {
+    const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${findBook}`);
+    setBookList(result.data.items);
+  };
+
+  useEffect(() => {
+    findBookName();
+  }, [findBook]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" name="text" onChange={(event) => setFindBook(event.target.value)} value={findBook} />
+      <ul>
+        {bookList.map((book) => {
+          return (
+            <>
+              <li key={book.id}>{book.volumeInfo.title}</li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
